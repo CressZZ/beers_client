a2023f2b10e5f785afa8b67f52143f92e0fc2869
CressZZ <dktsjssnrnsi@gmail.com>
2018-11-10 09:31:42 +0900

    react-router 초기 세팅

---
+++ src/pages/Beers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Beers = () => {
+    return (
+        <div>
+            <h2>
+                맥주리스트
+            </h2>
+        </div>
+    );
+};
+
+export default Beers;
