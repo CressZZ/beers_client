a2ae214749a7b86018c443e2d7f53350212361d7
CressZZ <dktsjssnrnsi@gmail.com>
2018-11-11 04:03:30 +0900

    주석 정리

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -18,7 +18,7 @@ function beers(state = [], action) {
 }
 
 /**
- * 태그 관련 reducer
+ * DB에 있는 모든 태그 관련 reducer
  */
 function tags(state = [], action) {
 
@@ -31,6 +31,9 @@ function tags(state = [], action) {
   }
 }
 
+/**
+ * 선택된 태그 관련 reducer
+ */
 function selctedTagsKey(state = [], action){
   switch (action.type) {
     // 태그 토글
@@ -40,32 +43,6 @@ function selctedTagsKey(state = [], action){
      return state;
   }
 }
-
-// function selctedTagsKey(state = [], action){
-//   switch (action.type) {
-//     // 태그 토글
-//     case TOGGLE_TAG:
-//       let _state = state.slice(0);
-//       _state = toggleTag(_state, action.tag)
-//       return _state;
-//     default:
-//      return state;
-//   }
-// }
-
-/**
- * 태그 토글
- */
-// function toggleTag(selectedTags, nowTag){
-//   let index = selectedTags.indexOf(nowTag)
-//   if(index > -1){
-//     selectedTags.splice(index, 1)
-//   }else{
-//     selectedTags.push(nowTag)
-//   }
-//   console.log(dispatch)
-//   return selectedTags;
-// }
 
 function page(state = {now:0, length:5}, action) {
   switch (action.type) {
