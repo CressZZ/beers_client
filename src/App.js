ddb4336d175423289ddf2b294e6745c8de327e60
CressZZ <dktsjssnrnsi@gmail.com>
2018-11-11 15:02:23 +0900

    header 를 라우트안에 컴포넌 트로 옮김

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Home, Beers, Cart } from './pages';
 import {Route } from 'react-router-dom'
-import Header from './Header.js'
 
 export default class App extends Component {
     render() {
         return (
                 <div>
-                    <Header />
                     <div className="container">
                     <Route exact path="/" component={Home} />
                     <Route exact path="/beer_list" component={Beers} />
