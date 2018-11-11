d579fbd1021e26b159a2cf204169af01cdb6dd91
CressZZ <dktsjssnrnsi@gmail.com>
2018-11-11 13:27:29 +0900

    Beer 스타일 수정

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -11,38 +11,35 @@ import BeerBtnBoxBeers from './BeerBtnBoxBeers';
  * 함수형으로 만든  BeerBtnBoxCart.js 
  */
 const Beer = (props) => {
+    const {pageName, beer, cartCnt, onClick} = props;
+    let beerBtnBox;
+    if(pageName === "cart"){
+        beerBtnBox = <BeerBtnBoxCart beer={beer} cartCnt={cartCnt} onClick={onClick}/>
+    }else if((pageName === "beers")){
+        beerBtnBox = <BeerBtnBoxBeers beer={beer} cartCnt={cartCnt} onClick={onClick}/>
+    }
+    return(
 
-   
-        const {pageName, beer, cartCnt, onClick} = props;
-        let beerBtnBox;
-        if(pageName === "cart"){
-            beerBtnBox = <BeerBtnBoxCart beer={beer} cartCnt={cartCnt} onClick={onClick}/>
-        }else if((pageName === "beers")){
-            beerBtnBox = <BeerBtnBoxBeers beer={beer} cartCnt={cartCnt} onClick={onClick}/>
-        }
-        return(
-            <div className ="beer__item">
-                <div className ="beer__top">
-                    <div className ="beer__img">
-                        <img src={`${beer.image}`} alt={`${beer.name}`} />
-                    </div>
-                    <div className ="beer__info">
-                        <p className ="beer__name"> {beer.name}</p>
-                        <p className ="beer__tags"> {beer.tags.join(', ')}</p>
-                        <p className ="beer__price"> {common.comma(beer.price)} <em>원</em></p>
-                        <div className ="beer__count">
-                            <p>재고 <em>{common.comma(beer.stock)}</em></p>
-                            <p>수량 <em>{common.comma(cartCnt)}</em></p>
-                        </div>
-                    </div>
+        <div className ="beer__item">
+            <div className ="beer__top">
+                <div className ="beer__img">
+                    <img src={`${beer.image}`} alt={`${beer.name}`} />
                 </div>
-                <div className ="beer__bottom">
-                    {beerBtnBox}
+                <div className ="beer__info">
+                    <p className ="beer__name"> {beer.name}</p>
+                    <p className ="beer__tags"> {beer.tags.join(', ')}</p>
+                    <p className ="beer__price"> {common.comma(beer.price)} <em>원</em></p>
+                    <div className ="beer__count">
+                        <p className={`${pageName === "cart" ? 'hide' : ''}`}>재고 <em>{common.comma(beer.stock)}</em></p>
+                        <p>수량 <em>{common.comma(cartCnt)}</em></p>
+                    </div>
                 </div>
             </div>
-        )
-
-    
+            <div className ="beer__bottom">
+                {beerBtnBox}
+            </div>
+        </div>
+    )
 };
 
 Beer.propTypes = {
